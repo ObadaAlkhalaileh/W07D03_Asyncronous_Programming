ed14b0f7bba6303254137b373f3b52ef776144b6
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-12 01:36:23 +0400

    pulse 3

--- app.js
+++ app.js
@@ -5,19 +5,30 @@ let PORT = 3000
 
 app.use(express.json());
 
+//Q1
 const fs = require("fs");
 const axios = require("axios");
 
-
+//Q2
 const readFile = () => {
     let content;
     fs.readFile("./data.txt", (err, data) => {
         if (err) throw err;
         content = data.toString();
+        console.log(content)
     });
     return content
 };
+readFile()
 
+//Q3
+const writeFile = () => {
+    fs.writeFile("text.txt", " A new file has been created", (err) => {
+        if (err) throw err;
+        console.log("The file has been created");
+    });
+};
+writeFile()
 
 app.get("/", (req, res) => {
     res.status(200)
