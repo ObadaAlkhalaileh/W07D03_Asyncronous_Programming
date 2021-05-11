dce59828741bfbdab32de612e6615a82c95af0d8
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-12 01:29:01 +0400

    pulse 2

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const fs = require("fs");
 const axios = require("axios");
 
 
+const readFile = () => {
+    let content;
+    fs.readFile("./data.txt", (err, data) => {
+        if (err) throw err;
+        content = data.toString();
+    });
+    return content
+};
+
+
 app.get("/", (req, res) => {
     res.status(200)
 
